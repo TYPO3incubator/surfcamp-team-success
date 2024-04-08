c64b1593644a99d295ec84a08656a4047cb35934
Arber Sala <sala@contenance.de>
2024-04-08 11:35:00 +0100

    [FEATURE] customized backend login screen

---
+++ local_packages/success/ext_localconf.php
@@ -0,0 +1,10 @@
+<?php
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend'] = [
+    'backendFavicon' => 'EXT:success/Resources/Public/Icons/surfcamp2.svg',
+    'backendLogo' => 'EXT:success/Resources/Public/Icons/surfcamp2.svg',
+    'loginBackgroundImage' => 'EXT:success/Resources/Public/Images/lapared.jpeg',
+    'loginLogo' => 'EXT:success/Resources/Public/Icons/surfcamp1.svg',
+    'loginHighlightColor' => '#84b0a5',
+    'loginFootnote' => 'Team Success',
+];
