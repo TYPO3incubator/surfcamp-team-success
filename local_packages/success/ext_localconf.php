0102b68488afd1bd4f231f815a2e4163f661f2f7
Arber Sala <sala@contenance.de>
2024-04-08 11:41:55 +0100

    [BUGFIX] fixed backend login background image size

--- local_packages/success/ext_localconf.php
+++ local_packages/success/ext_localconf.php
@@ -3,7 +3,7 @@
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend'] = [
     'backendFavicon' => 'EXT:success/Resources/Public/Icons/surfcamp2.svg',
     'backendLogo' => 'EXT:success/Resources/Public/Icons/surfcamp2.svg',
-    'loginBackgroundImage' => 'EXT:success/Resources/Public/Images/lapared.jpeg',
+    'loginBackgroundImage' => 'EXT:success/Resources/Public/Images/lapared.webp',
     'loginLogo' => 'EXT:success/Resources/Public/Icons/surfcamp1.svg',
     'loginHighlightColor' => '#84b0a5',
     'loginFootnote' => 'Team Success',
