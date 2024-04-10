c0b9be203e3cf3a90d7eb0086685942023d726a0
András Ottó <andras.otto@digital-zombies.com>
2024-04-10 18:25:11 +0100

    [BUGFIX] Undefined array keys issues

--- local_packages/success/Classes/Content/RecordEnricher.php
+++ local_packages/success/Classes/Content/RecordEnricher.php
@@ -49,52 +49,54 @@ class RecordEnricher
 
         foreach ($record->toArray() as $fieldName => $fieldValue) {
             $fieldConfiguration = $this->recordFactory->getFinalFieldConfiguration($fieldName, $tcaSchema, $subSchemaConfig);
-            switch ($fieldConfiguration['config']['type']) {
-                case 'file':
-                    /** @var FileCollector $fileCollector */
-                    $fileCollector = GeneralUtility::makeInstance(FileCollector::class);
-                    $fileCollector->addFilesFromRelation($mainType, $fieldName, $record->getRawRecord()->toArray());
-                    $resolvedProperties[$fieldName] = $fileCollector->getFiles();
-                    if ((int)($fieldConfiguration['config']['maxitems'] ?? 0) === 1) {
-                        $resolvedProperties[$fieldName] = reset($resolvedProperties[$fieldName]);
-                    }
-                    break;
-                case 'inline':
-                case 'select':
-                    if (!isset($fieldConfiguration['config']['foreign_table']) || !isset($fieldConfiguration['config']['foreign_field'])) {
+            if (isset($fieldConfiguration['config'])) {
+                switch ($fieldConfiguration['config']['type']) {
+                    case 'file':
+                        /** @var FileCollector $fileCollector */
+                        $fileCollector = GeneralUtility::makeInstance(FileCollector::class);
+                        $fileCollector->addFilesFromRelation($mainType, $fieldName, $record->getRawRecord()->toArray());
+                        $resolvedProperties[$fieldName] = $fileCollector->getFiles();
+                        if ((int)($fieldConfiguration['config']['maxitems'] ?? 0) === 1) {
+                            $resolvedProperties[$fieldName] = reset($resolvedProperties[$fieldName]);
+                        }
                         break;
-                    }
-                    $selectConfiguration = [
-                        'where' => $fieldConfiguration['config']['foreign_field'] . '=' . (int)$record->getRawUid(),
-                    ];
-                    if (isset($GLOBALS['TCA'][$fieldConfiguration['config']['foreign_table']]['ctrl']['sortby'])) {
-                        $selectConfiguration[$fieldName]['config']['sortBy'] = $GLOBALS['TCA'][$fieldConfiguration['config']['foreign_table']]['ctrl']['sortby'];
-                    } elseif (isset($GLOBALS['TCA'][$fieldConfiguration['config']['foreign_table']]['ctrl']['default_sortby'])) {
-                        $selectConfiguration[$fieldName]['config']['sortBy'] = $GLOBALS['TCA'][$fieldConfiguration['config']['foreign_table']]['ctrl']['default_sortby'];
-                    }
+                    case 'inline':
+                    case 'select':
+                        if (!isset($fieldConfiguration['config']['foreign_table']) || !isset($fieldConfiguration['config']['foreign_field'])) {
+                            break;
+                        }
+                        $selectConfiguration = [
+                            'where' => $fieldConfiguration['config']['foreign_field'] . '=' . (int)$record->getRawUid(),
+                        ];
+                        if (isset($GLOBALS['TCA'][$fieldConfiguration['config']['foreign_table']]['ctrl']['sortby'])) {
+                            $selectConfiguration[$fieldName]['config']['sortBy'] = $GLOBALS['TCA'][$fieldConfiguration['config']['foreign_table']]['ctrl']['sortby'];
+                        } elseif (isset($GLOBALS['TCA'][$fieldConfiguration['config']['foreign_table']]['ctrl']['default_sortby'])) {
+                            $selectConfiguration[$fieldName]['config']['sortBy'] = $GLOBALS['TCA'][$fieldConfiguration['config']['foreign_table']]['ctrl']['default_sortby'];
+                        }
 
-                    $cObj = GeneralUtility::makeInstance(ContentObjectRenderer::class);
-                    $cObj->start($record->getRawRecord()->toArray(), $record->getMainType());
-                    $recordFactory = new RecordFactory();
-                    $subRecords = GeneralUtility::makeInstance(RecordCollector::class, $recordFactory)->collect(
-                        $fieldConfiguration['config']['foreign_table'],
-                        $selectConfiguration,
-                        ContentSlideMode::None,
-                        $cObj
-                    );
-                    /** @var Record $subRecord */
-                    foreach ($subRecords as $subRecord) {
-                        $resolvedProperties[$fieldName][] = $this->createResolvedRecordFromRecord($subRecord)->toArray(true);
-                    }
+                        $cObj = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+                        $cObj->start($record->getRawRecord()->toArray(), $record->getMainType());
+                        $recordFactory = new RecordFactory();
+                        $subRecords = GeneralUtility::makeInstance(RecordCollector::class, $recordFactory)->collect(
+                            $fieldConfiguration['config']['foreign_table'],
+                            $selectConfiguration,
+                            ContentSlideMode::None,
+                            $cObj
+                        );
+                        /** @var Record $subRecord */
+                        foreach ($subRecords as $subRecord) {
+                            $resolvedProperties[$fieldName][] = $this->createResolvedRecordFromRecord($subRecord)->toArray(true);
+                        }
 
-                    if ((int)($fieldConfiguration['config']['maxitems'] ?? 0) === 1) {
-                        $resolvedProperties[$fieldName] = reset($resolvedProperties[$fieldName]);
-                    }
+                        if ((int)($fieldConfiguration['config']['maxitems'] ?? 0) === 1) {
+                            $resolvedProperties[$fieldName] = reset($resolvedProperties[$fieldName]);
+                        }
 
-                    break;
-                case 'flex':
-                    $resolvedProperties[$fieldName] = GeneralUtility::makeInstance(FlexFormService::class)->convertFlexFormContentToArray($fieldValue);
-                    break;
+                        break;
+                    case 'flex':
+                        $resolvedProperties[$fieldName] = GeneralUtility::makeInstance(FlexFormService::class)->convertFlexFormContentToArray($fieldValue);
+                        break;
+                }
             }
         }
         return new ResolvedRecord($record, $resolvedProperties);
