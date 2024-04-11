792daec43e4616c1818449d48b11a8fdb3a8decc
André Buchmann <andy.schliesser@gmail.com>
2024-04-11 12:25:34 +0100

    [TASK] Refactor TCA

--- local_packages/success/ext_tables.sql
+++ local_packages/success/ext_tables.sql
@@ -1,16 +1,20 @@
 CREATE TABLE tt_content
 (
 	overline     varchar(255) DEFAULT '' NOT NULL,
-	header_style varchar(255) DEFAULT '' NOT NULL
+	header_style varchar(255) DEFAULT '' NOT NULL,
+	button_link  varchar(2048),
+	button_label varchar(255) DEFAULT '' NOT NULL,
+	button_style varchar(255) DEFAULT '' NOT NULL
 );
 
 CREATE TABLE tx_success_card
 (
-	headline  varchar(255) DEFAULT '' NOT NULL,
-	text      text,
-	media     int(11) DEFAULT '0' NOT NULL,
--- 	link      varchar(2048),
--- 	link_text varchar(255) DEFAULT '' NOT NULL
+	headline     varchar(255) DEFAULT '' NOT NULL,
+	text         text,
+	media        int(11) DEFAULT '0' NOT NULL,
+	button_link  varchar(2048),
+	button_label varchar(255) DEFAULT '' NOT NULL,
+	button_style varchar(255) DEFAULT '' NOT NULL
 );
 
 CREATE TABLE tx_success_number_item
