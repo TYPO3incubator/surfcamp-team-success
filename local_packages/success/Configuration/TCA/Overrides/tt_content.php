03e37c5807b8f94441965298ae6f540ead8ea84d
André Buchmann <andy.schliesser@gmail.com>
2024-04-14 14:03:06 +0200

    [TASK] Style copyright

--- local_packages/success/Configuration/TCA/Overrides/tt_content.php
+++ local_packages/success/Configuration/TCA/Overrides/tt_content.php
@@ -738,3 +738,20 @@ $GLOBALS['TCA']['tt_content']['types']['success_topbar'] = [
 $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['success_topbar'] = 'content-header';
 
 $GLOBALS['TCA']['tt_content']['types']['form_formframework']['previewRenderer'] = \Surfcamp\Success\Hooks\FormPagePreviewRenderer::class;
+
+// Copright Images
+$GLOBALS['TCA']['tt_content']['types']['copyright_images'] = [
+    'showitem' => '
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+    --palette--;;general,
+    --palette--;;headers,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:appearance,
+    --palette--;;frames,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+    --palette--;;language, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+    --palette--;;hidden,
+    --palette--;;access,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes, rowDescription,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+    ',
+];
