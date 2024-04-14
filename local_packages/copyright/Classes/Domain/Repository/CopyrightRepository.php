611d17eb7deb79b24512bd27f8d617501210409d
André Buchmann <andy.schliesser@gmail.com>
2024-04-14 14:36:44 +0200

    [TASK] Refactor controller and repository

--- local_packages/copyright/Classes/Domain/Repository/CopyrightRepository.php
+++ local_packages/copyright/Classes/Domain/Repository/CopyrightRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Surfcamp\Copyright\Domain\Repository;
 
+use Doctrine\DBAL\Exception;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
@@ -11,34 +12,41 @@ use TYPO3\CMS\Core\Domain\Repository\PageRepository;
 use TYPO3\CMS\Core\Resource\FileRepository;
 use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Persistence\Repository;
 
-class CopyrightRepository extends Repository
+readonly class CopyrightRepository
 {
+    public function __construct(
+        protected PageRepository $pageRepository,
+        protected FileRepository $fileRepository,
+        protected ConnectionPool $connectionPool
+    )
+    {
+    }
 
+    /**
+     * @throws Exception
+     */
     public function findBySite(Site $site): array
     {
         // first fetch all pages in a specific site
-        $pageRepository = GeneralUtility::makeInstance(PageRepository::class);
-        $pageIds = $pageRepository->getDescendantPageIdsRecursive($site->getRootPageId(), 4);
+        $pageIds = $this->pageRepository->getDescendantPageIdsRecursive($site->getRootPageId(), 4);
         $pageIds[] = $site->getRootPageId();
 
-        $qb = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_reference');
-        $qb->setRestrictions(GeneralUtility::makeInstance(FrontendRestrictionContainer::class));
-        $fileReferences = $qb
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable('sys_file_reference');
+        $queryBuilder->setRestrictions(GeneralUtility::makeInstance(FrontendRestrictionContainer::class));
+        $fileReferences = $queryBuilder
             ->select('*')
             ->from('sys_file_reference')
             ->where(
-                $qb->expr()->in('pid', $qb->createNamedParameter($pageIds, Connection::PARAM_INT_ARRAY)),
+                $queryBuilder->expr()->in('pid', $queryBuilder->createNamedParameter($pageIds, Connection::PARAM_INT_ARRAY)),
             )
             ->orderBy('pid')
             ->executeQuery()
             ->fetchAllAssociative();
 
-        $fileRepository = GeneralUtility::makeInstance(FileRepository::class);
         $result = [];
         foreach ($fileReferences as $fileReference) {
-            $fileObject = $fileRepository->findByUid($fileReference['uid_local']);
+            $fileObject = $this->fileRepository->findByUid($fileReference['uid_local']);
             if (array_key_exists($fileObject->getUid(), $result)) {
                 $result[$fileObject->getUid()]['pages'][$fileReference['pid']] = $fileReference['pid'];
             } else {
