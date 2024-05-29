5e21539d1001a4e64958be683a6bb91282d8e1a5
Nadja Neb <neb@contenance.de>
2024-05-29 09:29:23 +0200

    [TASK] create a back to top button by using global style definitions and user settings

---
+++ assets/js/back-to-top.js
@@ -0,0 +1,19 @@
+const backToTopButton = document.querySelector('.scroll-to-top');
+
+const topButtonObserver = new IntersectionObserver(() => {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+
+  backToTopButton.addEventListener('click', function(){
+    window.scrollTo({
+      top:      0,
+      behavior: 'smooth'
+    });
+  });
+});
+topButtonObserver.observe(backToTopButton)
